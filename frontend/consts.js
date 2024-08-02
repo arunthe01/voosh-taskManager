9f4f82ee68fbcd79480d88d5e9e0e9d41b629bdb
arunthe01 <djdjdjaaa999@gmail.com>
2024-08-02 15:34:00 +0530

    push

--- frontend/consts.js
+++ frontend/consts.js
@@ -1,5 +1,4 @@
-const BACKEND_URL = "http://localhost:8080";
-//"https://voosh-taskmanager.onrender.com";
+const BACKEND_URL = "https://voosh-taskmanager.onrender.com";
 //storing here for time being as facing .env file issues on production
 
 export default BACKEND_URL;
